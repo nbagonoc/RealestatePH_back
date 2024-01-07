5a00ee3b806ed5e27249a5017030299d3eb9d48f
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-01-07 18:37:22 +0800

    created new routes to check authenticated and admin role routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,28 +17,23 @@ use App\Http\Controllers\Auth\AuthController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/login', [AuthController::class, 'signin']);
 });
 
-// public routes
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'signin']);
 
-//hmm where
-
-// // admin routes
-// Route::group(['middleware' => ['auth:sanctum', 'admin'], 'prefix' => 'admin'], function () {
-//     Route::get('/dashboard', [AdminController::class, 'dashboard']);
-// });
+// admin routes
+Route::group(['middleware' => ['auth:api', 'admin'], 'prefix' => 'admin'], function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard']);
+});
 
 
-// // authenicated routes
-// Route::group(['middleware' => ['auth:sanctum'], 'prefix' => 'user'], function () {
-//     Route::get('/profile', [UserController::class, 'profile']);
-//     Route::get('/dashboard', [UserController::class, 'dashboard']);
-//     Route::post('/logout', [AuthController::class, 'logout']);
-//     Route::get('/', function (Request $request) {
-//         return $request->user();
-//     });
-// });
+// authenicated routes
+Route::group(['middleware' => ['auth:api'], 'prefix' => 'user'], function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/', function (Request $request) {
+        return $request->user();
+    });
+});
