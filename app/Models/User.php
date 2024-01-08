c34ba3f5cd275349f1eb019f7195a02b3f33933f
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-01-08 22:37:22 +0800

    Created listings

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable implements JWTSubject
     {
       return [];
     }
+
+    public function listings()
+    {
+      return $this->hasMany(Listing::class);
+    }
 }
---
+++ database/migrations/2024_01_08_135138_create_listings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['active', 'pending', 'closed'])->default('active');
+            $table->text('description');
+            $table->float('price');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state', 2);
+            $table->string('zip', 5);
+            $table->string('photo');
+            $table->enum('category', ['house', 'condo', 'townhouse', 'apartment', 'land', 'commercial'])->default('house');
+            $table->enum('type', ['sale', 'rent'])->default('sale');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->float('sqft');
+            $table->float('lot_size');
+            $table->integer('year_built');
+            $table->integer('parking');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
