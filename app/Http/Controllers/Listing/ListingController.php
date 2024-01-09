f97f0cdc46c5c7170bb90e2f97158bf1a7c21b2e
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-01-09 21:34:12 +0800

    implemented update and delete to listingcontroller

--- app/Http/Controllers/Listing/ListingController.php
+++ app/Http/Controllers/Listing/ListingController.php
@@ -14,11 +14,17 @@ class ListingController extends Controller
     public function index()
     {
         $activeListings = Listing::where('status', 'active')->get();
+
         $formattedListings = $activeListings->map(function ($listing) {
             return collect($listing)->except(['description', 'updated_at'])->all();
         });
 
-        return response()->json(['listings' => $formattedListings], 200);
+        //check if formattedListings is empty
+        if (!$formattedListings->isEmpty()) {
+            return response()->json(['listings' => $formattedListings], 200);
+        } else {
+            return response()->json(['message' => 'No listings found'], 404);
+        }
     }
 
     /**
@@ -35,19 +41,45 @@ class ListingController extends Controller
     public function show(string $id)
     {
         $listing = Listing::find($id);
+
         if ($listing) {
             return response()->json($listing->toArray(), 200);
         } else {
-            return response()->json('Listing not found', 404);
+            return response()->json(['message' => 'Listing not found'], 404);
         }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Listing $listing)
     {
-        //
+        if (!$listing) {
+            return response()->json(['message' => 'Listing not found'], 404);
+        }
+
+        $data = $request->validate([
+            'status' => 'required|string|in:active,pending,closed',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'address' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'required|string|size:2',
+            'zip' => 'required|string|size:5',
+            'photo' => 'required|string',
+            'category' => 'required|string|in:house,condo,townhouse,apartment,land,commercial',
+            'type' => 'required|string|in:sale,rent',
+            'bedrooms' => 'required|integer',
+            'bathrooms' => 'required|integer',
+            'sqft' => 'required|numeric',
+            'lot_size' => 'required|numeric',
+            'year_built' => 'required|integer',
+            'parking' => 'required|integer',
+        ]);
+
+        $listing->update($data);
+
+        return response()->json(['message' => 'Listing updated'], 200);
     }
 
     /**
@@ -55,6 +87,14 @@ class ListingController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $listing = Listing::find($id);
+
+        if (!$listing) {
+            return response()->json(['message' => 'Listing not found'], 404);
+        }
+
+        $listing->delete();
+
+        return response()->json(['message' => 'Listing deleted'], 200);
     }
 }
