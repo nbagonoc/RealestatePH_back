d3ee8ed7ce98c0484d302bf1530ce3fda49e4471
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-01-11 21:47:57 +0800

    can now get agent details

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    public function dashboard()
+    public function getAgent($id)
     {
-        return response()->json(['message' => 'Hello user!']);
+        // $agent = User::find($id)::where('role', 'agent')->first();
+        $agent = User::where(['id' => $id, 'role' => 'agent'])
+            ->select('id', 'name', 'email')
+            ->first();
+
+        return $agent
+        ? response()->json($agent, 200)
+        : response()->json(['error' => 'Agent not found'], 404);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,33 +18,37 @@ use App\Http\Controllers\Listing\ListingController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+// public auth routes
 Route::prefix('auth')->group(function () {
     Route::post('/signup', [AuthController::class, 'signup']);
     Route::post('/login', [AuthController::class, 'signin']);
 });
 
-
 // agent routes
-Route::group(['middleware' => ['auth:api', 'agent'], 'prefix' => 'agent'], function () {
-    Route::get('/dashboard', [AgentController::class, 'dashboard']);
-});
+// Route::group(['middleware' => ['auth:api', 'agent'], 'prefix' => 'agent'], function () {
+//     Route::get('/dashboard', [AgentController::class, 'dashboard']);
+// });
 
+// USER ROUTES
 // authenicated routes
-Route::group(['middleware' => ['auth:api'], 'prefix' => 'user'], function () {
-    Route::get('/dashboard', [UserController::class, 'dashboard']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/', function (Request $request) {
+Route::group(['middleware' => ['auth:api'], 'prefix' => 'users'], function () {
+    Route::get('/profile', function (Request $request) {
         return $request->user();
     });
+    Route::get('/{id}/agent', [UserController::class, 'getAgent']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+// authenicated routes
+// Route::group(['middleware' => ['auth:api', 'agent'], 'prefix' => 'agent'], function () {
+//     Route::get('/{id}/user', [UserController::class, 'getAgent']);
+// });
 
+// LISTINGS ROUTES
 // public listing routes
 Route::prefix('listings')->group(function () {
     Route::get('/', [ListingController::class, 'index']);
     Route::get('/{id}', [ListingController::class, 'show']);
 });
-
 // protected listing routes
 Route::group(['middleware' => ['auth:api', 'agent'], 'prefix' => 'listings'], function () {
     Route::post('/', [ListingController::class, 'store']);
@@ -53,11 +57,10 @@ Route::group(['middleware' => ['auth:api', 'agent'], 'prefix' => 'listings'], fu
     Route::delete('/{id}', [ListingController::class, 'destroy']);
 });
 
-// protected listing routes
+// LIKE LISTINGS ROUTES
+// authenicated Likes listing routes
 Route::group(['middleware' => ['auth:api'], 'prefix' => 'likes'], function () {
     Route::post('/{id}', [LikeController::class, 'likeListing']);
     Route::get('/', [LikeController::class, 'likedListings']);
     Route::delete('/{id}', [LikeController::class, 'unlikeListing']);
 });
-
-// Route::apiResource('listings', ListingController::class)->middleware('auth:api');
